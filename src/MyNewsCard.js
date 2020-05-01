6d6e9c587f53a496a246edba9ec91c2289980250
vforvishal12 <52970035+vforvishal12@users.noreply.github.com>
2020-05-01 14:47:02 +0530

    Add files via upload

--- src/MyNewsCard.js
+++ src/MyNewsCard.js
@@ -7,25 +7,17 @@ const MyNewsCard = ({articles}) => {
 
     return (
         <div>
-
-            {articles.map((article, index) => (
-                
-
-			 <Card className="bg-dark text-white">
+        
+        <Card className="bg-dark text-white">
 			  <Card.Img src="https://picsum.photos/200/300?grayscale" alt="Card image" />
 			  <Card.ImgOverlay>
-			    <Card.Title></Card.Title>
-			    <Card.Text>
-			    {article.insult}
-			    </Card.Text>
+			<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+			    <Card.Title class="card-title h1">
+			    {articles}
+			    </Card.Title>
 			   
 			  </Card.ImgOverlay>
 			</Card>
-				
-
-            ))} 
-             
-
         </div>
     )
 };
--- src/NewsAPI.js
+++ src/NewsAPI.js
@@ -13,14 +13,30 @@ class NewsAPI extends Component {
     };
 
     componentDidMount() {
-        fetch('https://evilinsult.com/generate_insult.php?lang=en&type=json')
+        var proxyUrl = 'https://cors-anywhere.herokuapp.com/',
+    targetUrl = 'https://evilinsult.com/generate_insult.php?lang=en&type=json'
+fetch(proxyUrl + targetUrl)
+  .then(blob => blob.json())
+  .then(data => {
+    //this.setState({ articles: data })
+
+    //console.table(data);
+    var keys = Object.values(data)
+    var key_to_use = keys[2];
+    //console.log(key_to_use);
+   // var b= JSON.stringify(data, null, 2);
+    this.setState({ articles: key_to_use})
+ //org    this.setState({ articles: JSON.stringify(data, null, 2) })
+    //document.querySelector("pre").innerHTML = JSON.stringify(data, null, 2);
+   // return data;
+  })
+        //fetch('https://evilinsult.com/generate_insult.php?lang=en&type=json')
             //
             //.then(res => res.text())          // convert to plain text
            // .then(text => console.log('sttt' + text))  // then log it out
-            .then(res => res.json())
-            .then((data) => {
-                this.setState({ articles: data })
-            })
+           // .then(res => res.json())
+           // .then((data) => { this.setState({ articles: data })
+           // })
             .catch(console.log)
     }
 }
